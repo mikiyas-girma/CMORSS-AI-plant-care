e308dc96802b79866272b94fd0c9c14887c4d12a
Rayane <rayanetoko@gmail.com>
2024-09-11 23:19:48 +0100

    feat(auth guard): add auth guard.
    This auth guard redirect user to the correct route in the event that user is already connected or not

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import Router from '@/routes'
-import { MediaQueryProvider } from '@/contexts/MediaQueryContext'
-import ContainerLayout from './gui/layouts/ContainerLayout'
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Router from '@/routes';
+import { MediaQueryProvider } from '@/contexts/MediaQueryContext';
+import ContainerLayout from '@/gui/layouts/ContainerLayout';
+import AuthGuard from '@/gui/components/AuthGuard';
 
 function App() {
 	return (
 		<MediaQueryProvider>
 			<BrowserRouter>
 				<ContainerLayout>
-					<Router />
+					<AuthGuard>
+						<Router />
+					</AuthGuard>
 				</ContainerLayout>
 			</BrowserRouter>
 		</MediaQueryProvider>
