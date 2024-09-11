6c7634aa528a7a0a627f80b0eef3c623a29fb982
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-09-11 09:05:33 +0100

    Hehe 😁 added poppins font import and extend font family in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
