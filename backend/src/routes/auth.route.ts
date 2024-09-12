311ceec647771979090e2648d5787f7c589babf4
Rayane <rayanetoko@gmail.com>
2024-09-12 22:22:36 +0100

    feat(auth check): add authentification check to verify if a client is authentified or not

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signin, signup } from '../controllers/auth.controllers.js';
+import { signin, signup, checkAuth } from '../controllers/auth.controllers.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/check", checkAuth);
 
 export default router;
