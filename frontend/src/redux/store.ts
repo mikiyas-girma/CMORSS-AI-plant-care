2aa20a0853062ffe3ee20f7689962f30ab8b0402
Prish20 <prish@students.uonbi.ac.ke>
2024-09-12 18:19:21 +0300

    Implement redux-toolkit for state management.

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,14 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
