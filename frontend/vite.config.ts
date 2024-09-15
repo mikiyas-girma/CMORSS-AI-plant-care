0799362f81d04784bc4792f7cdf95e8a96bb8baa
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-15 12:28:03 +0300

    configure Vite to use different proxy targets based on the environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+
+const apiURL = process.env.VITE_API_URL || "http://localhost:3000"
  
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: apiURL,
         secure: false,
+        changeOrigin: true,
       },
     }
   },
