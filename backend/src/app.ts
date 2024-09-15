62d3e744e7eb5fb9618f30d36cde39f851430149
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-16 01:51:19 +0300

    fixed cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://plantcare.mikegirma.tech'],
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
   })
 );
