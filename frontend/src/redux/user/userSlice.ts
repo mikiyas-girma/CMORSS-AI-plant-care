ab43341f83ffc1a65a6bf487efa48e4e2333375b
Rayane <rayanetoko@gmail.com>
2024-09-16 14:00:12 +0100

    fix(import): delete not necessary import

--- frontend/src/redux/user/userSlice.ts
+++ frontend/src/redux/user/userSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { User } from '@/types/user';
-import { persistor } from '../store';
 
 export interface UserState {
   currentUser: null | User;
